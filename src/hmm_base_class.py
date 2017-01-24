333f32d10c44029cd2c1c70283f08f52e44e18df
Alexander Skates <alexander.skates@googlemail.com>
2017-01-24 13:18:37 +0000

    Removing non-essential tensorflow

--- src/hmm_base_class.py
+++ src/hmm_base_class.py
@@ -1,14 +1,10 @@
-import tensorflow as tf
 import numpy as np
-from src.utilities import *
-
-_epsilon = 1.0e-8
+from src.np_utilities import dirichlet_expected_log_likelihood, dirichlet_lower_bound
 
 class _HMM():
     
     """
-    Hidden Markov model base class using Tensorflow.
-    Do not use directly, instead inherit from it.
+    Hidden Markov model base class. Do not use directly, instead inherit from it.
     """
     
     def __init__(self, obs, prior_pi, prior_A, init_pi=None, init_A=None):
@@ -35,40 +31,29 @@ class _HMM():
         
         """
         
-        with tf.name_scope('Scalar_Constants'):
-            self._K = len(prior_pi)
-            self._T, self._D = np.shape(obs)
+        self._K = len(prior_pi)
+        self._T, self._D = np.shape(obs)
+        self._obs = obs
         
-        with tf.name_scope('Data'):
-            self._obs = tf.constant(obs, dtype=tf.float32, name='observations')
+        #Prior hyperparams
+        self._pi_0 = prior_pi
+        self._A_0 = prior_A
+        
+        #Variational hyperparams
+        if init_pi is None:
+            self._var_pi = prior_pi / np.sum(prior_pi)
+        else:
+            self._var_pi = init_pi.copy()
             
-        with tf.name_scope('Prior_Hyperparams'):
-            self._pi_0 = tf.Variable(prior_pi, 
-                                    dtype=tf.float32, name='pi_0')
-            self._A_0 = tf.Variable(prior_A, 
-                                   dtype=tf.float32, name='A_0')
-                
-        with tf.name_scope('Variational_Hyperparams'):
-            if init_pi is None:
-                prior_pi = tf.cast(prior_pi, tf.float32)
-                self._var_pi = tf.Variable(
-                    prior_pi / tf.reduce_sum(prior_pi, keep_dims=True), 
-                    dtype=tf.float32, name='pi')
-            else:
-                self._var_pi = tf.Variable(init_pi, dtype=tf.float32, name='pi')
-                
-            if init_A is None:
-                prior_A = tf.cast(prior_A, tf.float32)
-                self._var_A = tf.Variable(
-                    prior_A / tf.reduce_sum(prior_A, axis=1, keep_dims=True),
-                    dtype=tf.float32, name='A')
-            else:
-                self._var_A = tf.Variable(init_A, dtype=tf.float32, name='A')
+        if init_A is None:
+            self._var_A = prior_A / np.sum(prior_A, axis=1)[:,np.newaxis]
+        else:
+            self._var_A = init_A.copy()
         
     
     def _fwd(self, obs=None):
         """
-        Compute the forward parameters as well as scaling parameters.
+        Compute the forward parameters.
         
         Parameters
         ==========
@@ -79,39 +64,15 @@ class _HMM():
         
         if obs is None:
             obs = self._obs
-
-        l_A = self._aux_A
-        l_l = self._ln_lik
         
         # Belief at t=0
-        with tf.name_scope('Forward_First_Step'):
-            # Calculate the values
-            l_alpha = self._aux_pi + l_l[0,:]
-            l_scale = tf.reciprocal(
-                tf.log(tf.reduce_sum(tf.exp(l_alpha), keep_dims=True)))
+        self._ln_alpha[0] = self._aux_pi + self._ln_lik[0]
             
-            # Update the variables
-            tf.scatter_update(
-                    self._ln_scale, tf.constant([0]), tf.expand_dims(l_scale, 0))
-            tf.scatter_update(
-                self._ln_alpha, tf.constant([0]), tf.expand_dims(l_alpha + l_scale, 0))
-
         # Propagate belief
         for t in range(1, self._T):
-            
-            with tf.name_scope('Forward_Time_Step_{}'.format(t)):
-                # N.B. transpose transition matrix for broadcasting addition
-                # across all values of k
-                l_alpha = log_sum_exp(
-                    self._ln_alpha[t-1, :] + tf.transpose(l_A)) + l_l[t,:]
-                l_scale = tf.reciprocal(
-                    tf.log(tf.reduce_sum(tf.exp(l_alpha), keep_dims=True)))
-
-                # Update forward matrix
-                self._ln_scale = tf.scatter_update(
-                    self._ln_scale, tf.constant([t]), tf.expand_dims(l_scale, 0))
-                self._ln_alpha = tf.scatter_update(
-                    self._ln_alpha, tf.constant([t]), tf.expand_dims(l_alpha + l_scale, 0))
+            # Log sum exp trick for matrix multiplication in log space
+            self._ln_alpha[t] = np.logaddexp.reduce(
+                self._ln_alpha[t-1] + self._aux_A.T, axis=1) + self._ln_lik[0]
 
                 
     def _bwd(self, obs=None):
@@ -127,28 +88,13 @@ class _HMM():
         
         if obs is None:
             obs = self._obs
-
-        l_A = self._aux_A
-        l_l = self._ln_lik
         
-        with tf.name_scope('Backwards_First_Step'):
-            # At first backwards step, scaled beta = 1 * scale[T-1]
-            b_0 = np.array([1] * self._K) * self._ln_scale[self._T-1]
-            self._ln_beta = tf.scatter_update(
-                self._ln_beta, tf.constant([self._T-1]), tf.expand_dims(b_0,0))
+        # At first backwards step, beta = 1, so log beta = 0
+        self._ln_beta[self._T-1] = 0
 
         for t in range(self._T-2, -1, -1):
-            
-            with tf.name_scope('Backwards_Time_Step_{}'.format(t)):
-                
-                # N.B. transpose transition matrix for broadcasting addition
-                # across all values of k
-                l_beta = log_sum_exp(l_A +  self._ln_beta[t+1, :] + l_l[t+1,:])
-
-                # Update backwards matrix
-                self._ln_beta = tf.scatter_update(
-                    self._ln_beta, tf.constant([t]),
-                    tf.expand_dims(l_beta + self._ln_scale[t], 0))
+            self._ln_beta[t] = np.logaddexp.reduce(
+                self._aux_A + self._ln_beta[t+1] + self._ln_lik[t+1], axis=1)
         
         
     def _local_update(self, obs=None):
@@ -165,25 +111,22 @@ class _HMM():
             not performing batched inference, as defaults to self._obs
         """
         
-        with tf.name_scope('Local_Update'):
-            if obs is None:
-                obs = self._obs
+        if obs is None:
+            obs = self._obs
         
-            self._aux_pi = tf.assign(self._aux_pi,
-                dirichlet_expected_log_likelihood(self._var_pi))
+        self._aux_pi = dirichlet_expected_log_likelihood(self._var_pi)
+        self._aux_A = dirichlet_expected_log_likelihood(self._var_A)
+        self._ln_lik = self._emission_log_likelihood(obs)
         
-            self._aux_A = tf.assign(self._aux_A,
-                dirichlet_expected_log_likelihood(self._var_A))
+        # Update forward, and backward values
+        self._fwd()
+        self._bwd()
         
-            # Compute log-likelihoods
-            self._ln_lik = self._emission_log_likelihood(obs)
-        
-            # Update forward, and backward values
-            self._fwd()
-            self._bwd()
-        
-            # Update the marginal belief
-            self._marginal.assign(tf.exp(self._ln_alpha + self._ln_beta))
+        # Update the marginal belief
+        self._marginal = self._ln_alpha + self._ln_beta
+        self._marginal -= np.max(self._marginal, axis=1)[:, np.newaxis]
+        self._marginal = np.exp(self._marginal)
+        self._marginal /= np.sum(self._marginal, axis=1)[:, np.newaxis]
         
         
     def _emission_log_likelihood(self):
@@ -205,18 +148,12 @@ class _HMM():
         Compute variational lower bound
         """
         
-        elbo = tf.constant(0, dtype=tf.float32)
+        elbo = 0
         
         elbo += dirichlet_lower_bound(self._pi_0, self._var_pi)
-        elbo += tf.reduce_sum(
-            dirichlet_lower_bound(self._A_0, self._var_A))
+        elbo += dirichlet_lower_bound(self._A_0, self._var_A)
         elbo += self._emission_lower_bound()
         # Entropy of states
-        elbo += -tf.reduce_sum(self._ln_scale)
+        elbo += np.sum(np.logaddexp.reduce(self._ln_alpha, axis=1))
         
-        return tf.squeeze(elbo)
-        
-    
-    def _summarise(self, name, value):
-        #log for tensorboard
-        tf.summary.scalar(name, value)
+        return elbo
